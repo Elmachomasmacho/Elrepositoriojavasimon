9bec553cbb9bbf09ce4b4972cfe3942893badb72
CCB0\BACO <alangonzalez9407@gmail.com>
2023-09-21 15:52:24 -0600

    Add info section

--- CarpetaparaProyecto/my-site/src/app/page.js
+++ CarpetaparaProyecto/my-site/src/app/page.js
@@ -19,6 +19,34 @@ export default function Home() {
           </h1>
         </div>
       </header>
+      <section className={styles.infoSection}>
+        <img
+          src="/bale.jpg"
+          alt="Imagen"
+          className={styles.image}
+        />
+        <div className={styles.infoContainer}>
+          <span className={styles.title + " " + styles.yellowText}>Josh</span>
+          <br />
+          <span className={styles.title}>Carlson</span>
+
+          <div className={styles.list}>
+            <ul>
+              <li>
+                <span className={styles.grayText}>Age: </span> 27
+              </li>
+              <li>
+                <span className={styles.grayText}>Nationality: </span> German
+              </li>
+              <li>
+                <span className={styles.grayText}>Skill set: </span> Project Management and Financial Performance
+              </li>
+              <li>
+                <span className={styles.grayText}>Language: </span> English, German
+              </li>
+            </ul> </div>
+        </div>
+      </section>
     </main>
   );
 }
